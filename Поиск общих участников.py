213ee26364d816968349d3fae664d1483e9e2c69
anferovajulia <149067593+anferovajulia@users.noreply.github.com>
2023-11-12 21:43:13 +0300

    Add files via upload

---
+++ Поиск общих участников.py
@@ -0,0 +1,13 @@
+# TODO Напишите функцию find_common_participants
+
+def find_common_participants(group1, group2, div=','):
+    group1 = group1.split(div)
+    group2 = group2.split(div)
+    group1 = set(group1)
+    group2 = set(group2)
+    group3 = group1.intersection(group2)
+    return sorted (list(group3))
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+print(find_common_participants(participants_first_group, participants_second_group,'|'))
+# TODO Провеьте работу функции с разделителем отличным от запятой
